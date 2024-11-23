2369a698f4387255c4aba84a0b8ad93baaf1e574
pichead <61868523+pichead@users.noreply.github.com>
2024-11-23 19:25:24 +0700

    chang app port to  3333

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { env } from 'utils/constant';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(env.appPort)
-  await app.listen(env.appPort);
+  await app.listen(3333);
   console.log('app listening on : http://localhost:'+env.appPort)
 }
 bootstrap();
