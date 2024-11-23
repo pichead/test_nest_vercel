62ca180892a530adb2c7f67b0ad3a742ae755ca3
pichead <61868523+pichead@users.noreply.github.com>
2024-11-23 19:32:44 +0700

    remove use env1

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { env } from 'utils/constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(env.appPort)
   await app.listen(3333);
   console.log('app listening on : http://localhost:'+env.appPort)
 }
