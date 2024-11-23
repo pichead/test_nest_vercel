13777ddb721d1632b21a092e75cf96811d7bb2be
pichead <61868523+pichead@users.noreply.github.com>
2024-11-23 19:09:28 +0700

    add v2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
