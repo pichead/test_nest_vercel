3d761666aabd676e7431b3d87f36ca67a7f258c0
pichead <61868523+pichead@users.noreply.github.com>
2024-11-23 22:05:55 +0700

    change prismaclient

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -199,7 +199,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
