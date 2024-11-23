edbca8d8584edeb0e511817f3d5de400b779d93e
pichead <61868523+pichead@users.noreply.github.com>
2024-11-23 20:44:20 +0700

    remove v1/auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './v1/auth/auth.module';
 import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [AuthModule, AdminModule],
+  imports: [AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
