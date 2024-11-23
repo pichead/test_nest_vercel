ab6b1cb135cd145c43a3d09362748e8f8a96bed6
pichead <61868523+pichead@users.noreply.github.com>
2024-11-23 19:11:18 +0700

    fix v2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { env } from 'utils/constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(env.appPort);
+  await app.listen(3000);
 }
 bootstrap();
