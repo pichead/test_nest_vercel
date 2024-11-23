538f5ea97c78f0c17d5bae05a224f28f670d7e1f
pichead <61868523+pichead@users.noreply.github.com>
2024-11-23 20:00:47 +0700

    set env to constant

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ENV } from './constant';
+import { env2 } from 'utils/constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(ENV.appPort);
+  await app.listen(env2.appPort);
 }
 bootstrap();
